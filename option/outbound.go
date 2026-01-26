b7f2191eede1d0cb52b0101393ce621beed836f4
hiddify <114227601+hiddify-com@users.noreply.github.com>
2026-01-26 20:36:31 +0330

    new: add xray option stubs

--- option/outbound.go
+++ option/outbound.go
@@ -109,6 +109,8 @@ type AbstractDialerOptions struct {
 
 	// Deprecated: migrated to domain resolver
 	DomainStrategy DomainStrategy `json:"domain_strategy,omitempty" schema:"omit"`
+
+	TLSFragment TLSFragmentOptions `json:"tls_fragment,omitempty"` //hiddify
 }
 
 type _DomainResolveOptions struct {
