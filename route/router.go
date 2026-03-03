2e65b0e7f2f4a96844fa05f3076156704e46c74d
世界 <i@sekai.icu>
2026-03-03 20:59:13 +0800

    Add MAC and hostname rule items

--- route/router.go
+++ route/router.go
@@ -162,6 +162,7 @@ func (r *Router) Start(stage adapter.StartStage) error {
 		r.ruleSetUpdater = R.NewRuleSetUpdater(r.ctx, r.ruleSets)
 		r.network.Initialize(r.ruleSets)
 		needFindProcess := r.needFindProcess
+		needFindNeighbor := r.needFindNeighbor
 		for _, ruleSet := range r.ruleSets {
 			metadata := ruleSet.Metadata()
 			if metadata.ContainsProcessRule {
@@ -195,6 +196,24 @@ func (r *Router) Start(stage adapter.StartStage) error {
 			processCache := common.Must1(freelru.New[processCacheKey, processCacheEntry](256, maphash.NewHasher[processCacheKey]().Hash32, true))
 			processCache.SetLifetime(200 * time.Millisecond)
 			r.processCache = processCache
+		}
+		r.needFindNeighbor = needFindNeighbor
+		if needFindNeighbor {
+			monitor.Start("initialize neighbor resolver")
+			resolver, err := newNeighborResolver(r.logger, r.leaseFiles)
+			monitor.Finish()
+			if err != nil {
+				if err != os.ErrInvalid {
+					r.logger.Warn(E.Cause(err, "create neighbor resolver"))
+				}
+			} else {
+				err = resolver.Start()
+				if err != nil {
+					r.logger.Warn(E.Cause(err, "start neighbor resolver"))
+				} else {
+					r.neighborResolver = resolver
+				}
+			}
 		}
 	case adapter.StartStatePostStart:
 		for i, rule := range r.rules {
